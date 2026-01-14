dcc961ac8e82bf455d9fa13f3017a03d85f96dad
DavidB <davidvenga@yahoo.com>
2026-01-14 17:05:46 -0500

    Spots Stage 6

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,29 @@
-
+const initialCards = [
+  {
+    name: "Val Thorens",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+  {
+    name: "Restaurant terrace",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
+  },
+  {
+    name: "An outdoor cafe",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
+  },
+  {
+    name: "A very long bridge, over the forest and through the trees",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
+  },
+  {
+    name: "Tunnel with morning light",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
+  },
+  {
+    name: "Mountain house",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+];
 
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
@@ -53,7 +78,6 @@ function handledEditProfileSubmit(evt) {
   profileNameEl.textContent = profileNameInput.value;
   profileDescriptionEl.textContent = profileDescriptionInput.value;
   closeModal(editProfileModal);
-  //editProfileModal.classList.remove("modal_is-opened");
 }
 
 editProfileForm.addEventListener("submit", handledEditProfileSubmit);
@@ -63,8 +87,11 @@ function handleAddCardSubmit(evt) {
   console.log(imageInput.value);
   console.log(captionInput.value);
   closeModal(newPostModal);
-  //newPostModal.classList.remove("modal_is-opened");
 }
 
 addCardForm.addEventListener("submit", handleAddCardSubmit);
 
+initialCards.forEach(function (item) {
+  console.log(item.name);
+  console.log(item.link);
+});
